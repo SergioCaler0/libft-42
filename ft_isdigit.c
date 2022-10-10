b7ff043fce7bcc1f7b7f1e8b06a39389cd409574
Sergio Calero Sanchez <scalero-@c2r11s6.42madrid.com>
2022-10-10 13:48:54 +0200

    segunda funcion

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+int ft_isdigit(char *c)
+{
+    int i;
+    int salida;
+
+    i = 0;
+    salida = 1;
+    while (c[i] != '\0')
+    {
+        if (c[i] >= 48 && c[i] <= 57)
+             salida = 1;
+        else
+        {
+             salida = 0;
+             break;
+        }
+		i++;
+    }
+	return salida;
+}
+
+/*int main()
+{
+
+	char destino[] = "123j";
+	ft_isdigit(destino);
+    printf("%d", ft_isdigit(destino));
+}*/
 
